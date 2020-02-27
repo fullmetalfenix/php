b9295776fac785ec7178b3bcae6d4cca7c4a372f
Jason <Jpihlk@yahoo.com>
2020-02-26 23:05:07 -0500

    added slider

--- index.php
+++ index.php
@@ -54,6 +54,32 @@ endforeach;
             </div>
         </div>
 
+
+<div class="container-fluid my-max-width-container">
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <p class="d-block w-100 knockout-text" >Slide #1.</p>
+    </div>
+    <div class="carousel-item">
+    <p class="d-block w-100 knockout-text" >Slide #2.</p>
+    </div>
+    <div class="carousel-item">
+    <p class="d-block w-100 knockout-text" >Slide #3.</p>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</div>
+
     </div>        
 </body>
 </html>
