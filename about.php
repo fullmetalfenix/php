5ca5d1a191534246c5ced68abe32206c8f291931
J Pihlk <jpihlk@yahoo.com>
2020-03-11 13:17:18 -0400

    done.

--- about.php
+++ about.php
@@ -8,12 +8,12 @@ include 'header.php';
 
         <div class="subpage-content-container">
             <h1 class="knockout-text">About Us</h1>
-            <div class="about-p-container">
-            <p class="border-bottom rounded">  This project was created from scratch to demo some PHP and Bootstrap as it seems to be in alot of  
+            <div class="about-p-container border-bottom rounded">
+            <p > <span> This project was created from scratch to demo some PHP and Bootstrap as it seems to be in alot of  
             "Nice to have" sections. I'm just demo-ing some of the standard bootstrap stuff and some PHP for pulling 
             together a site with a few templates, partials etc. I've done a few things here I wouldn't recomend such 
-            as adding an unnessisary loop for the homepage and not expanded / abstracted as much as I might have if this wasn't a 
-            very basic demo.
+            as adding an unnessisary loop for the homepage and not expanded / abstracted as much as I might have if this wasn't a <em>very basic</em> demo.</span>
+            </p>
             </div>
         
         </div>
